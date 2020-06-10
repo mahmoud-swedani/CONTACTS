98c2fbdd377e2a130f3b906a6aff2b851f07de78
mahmoud-swedani <mahmoudkasimswedani@gmail.com>
2020-06-11 02:20:27 +0300

    Accessing Django Admin Site

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from app.models import Contact
 # Register your models here.
+
+
+
+
+
+
+
+admin.site.register(Contact)
--- app/models.py
+++ app/models.py
@@ -4,11 +4,15 @@ from django.db import models
 class Contact(models.Model):
     name = models.CharField(max_length=20)
     emile = models.EmailField(max_length=100)
-    phone = models.ImageField()
-    info = models.CharField(max_length=50)
+    phone = models.IntegerField(("phone"))
+    info = models.CharField(max_length=30)
     gender = models.CharField(max_length=50, choices=(
-        ('male', 'Male')
+        ('male', 'Male'),
         ('famle', 'famle')
     ))
     image = models.ImageField(upload_to='images/',blank=True)
     date_add = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
