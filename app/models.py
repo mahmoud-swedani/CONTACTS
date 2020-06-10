36c66ae2d18978cf3d4876a0cb76dd6bf023834f
mahmoud-swedani <mahmoudkasimswedani@gmail.com>
2020-06-11 00:44:42 +0300

    Working with Models & Media files &fixes require

--- app/models.py
+++ app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    name = models.CharField(max_length=20)
+    emile = models.EmailField(max_length=100)
+    phone = models.ImageField()
+    info = models.CharField(max_length=50)
+    gender = models.CharField(max_length=50, choices=(
+        ('male', 'Male')
+        ('famle', 'famle')
+    ))
+    image = models.ImageField(upload_to='images/',blank=True)
+    date_add = models.DateField(auto_now_add=True)
--- contact/settings.py
+++ contact/settings.py
@@ -121,3 +121,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+
+# setting media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
